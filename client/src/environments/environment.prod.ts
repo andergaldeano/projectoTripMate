034938c5347affd9cde192b860b54eca70e6d8c6
Ander Galdeano <andergaldeano@gmail.com>
2017-10-13 10:39:13 +0200

    Second Fix

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,20 @@
+//El Viejo
+// export const environment = {
+//   production: true,
+//   BASEURL: '',
+//   firebase: {
+//     apiKey: "AIzaSyCVTosLLjsm_Q8g2G98lH1rMBt7cuAaMKM",
+//     authDomain: "tripmate-89b3c.firebaseapp.com",
+//     databaseURL: "https://tripmate-89b3c.firebaseio.com",
+//     projectId: "tripmate-89b3c",
+//     storageBucket: "",
+//     messagingSenderId: "544411279165"
+//   }
+// };
+//EL nuevo CHUNG
 export const environment = {
-  production: true,
-  BASEURL: '',
+  production: false,
+  BASEURL: 'http://localhost:3000',
   firebase: {
     apiKey: "AIzaSyCVTosLLjsm_Q8g2G98lH1rMBt7cuAaMKM",
     authDomain: "tripmate-89b3c.firebaseapp.com",
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,9 +3,24 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+
+//Original
+// export const environment = {
+//   production: false,
+//   BASEURL: 'http://localhost:3000',
+//   firebase: {
+//     apiKey: "AIzaSyCVTosLLjsm_Q8g2G98lH1rMBt7cuAaMKM",
+//     authDomain: "tripmate-89b3c.firebaseapp.com",
+//     databaseURL: "https://tripmate-89b3c.firebaseio.com",
+//     projectId: "tripmate-89b3c",
+//     storageBucket: "",
+//     messagingSenderId: "544411279165"
+//   }
+// };
+//Nuevo Chungo
 export const environment = {
-  production: false,
-  BASEURL: 'http://localhost:3000',
+  production: true,
+  BASEURL: '',
   firebase: {
     apiKey: "AIzaSyCVTosLLjsm_Q8g2G98lH1rMBt7cuAaMKM",
     authDomain: "tripmate-89b3c.firebaseapp.com",
